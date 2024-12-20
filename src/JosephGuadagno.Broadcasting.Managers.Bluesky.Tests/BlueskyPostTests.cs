6917bc6a047669f2f87d81bb5ba9b1a2a2f32d02
Joseph Guadagno <jguadagno@hotmail.com>
2024-12-19 18:17:54 -0700

    Updated the packages and tweaked the Bluesky tests

--- src/JosephGuadagno.Broadcasting.Managers.Bluesky.Tests/BlueskyPostTests.cs
+++ src/JosephGuadagno.Broadcasting.Managers.Bluesky.Tests/BlueskyPostTests.cs
@@ -22,10 +22,29 @@ public class BlueskyPostTests
     }
     
     [Fact]
-    public async Task SendBlueskyPost_Success()
+    public async Task SendBlueskyPostText_Success()
     {
         // Arrange
         var message = "Test message - Testing in Production";
+        
+        // Act
+        var response = await _blueskyManager.PostText(message);
+        
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.Cid);
+        
+        // Clean up
+        await _blueskyManager.DeletePost(response.StrongReference);
+    }
+    
+    [Fact]
+    public async Task SendBlueskyPostText_ICYMI_Success()
+    {
+        // Arrange
+        var message = "ICYMI: (07/12/2024): \"My JetBrains Rider Plugins — July 2024 Edition\" RPs and feedback are always appreciated! ";
+        message += "https://jjg.me/3LmnPiF ";
+        message += "#JetBrains #Plugin #presenting #Rider";
         
         // Act
         var response = await _blueskyManager.PostText(message);
@@ -50,12 +69,14 @@ public class BlueskyPostTests
         {
             "Azure", "Identity", "WebAPI", "MSAL", "ManagedIdentity", "Entra"
         };
-        
-        postBuilder.Append(" " + new Link(shortenedUrl, shortenedUrl));
+
+        postBuilder.Append(" ");
+        postBuilder.Append(new Link(shortenedUrl, shortenedUrl));
         
         foreach (var hashtag in hashTags)
         {
-            postBuilder.Append(" " + new HashTag(hashtag));
+            postBuilder.Append(" ");
+            postBuilder.Append(new HashTag(hashtag));
         }
             
         // Act
@@ -82,7 +103,8 @@ public class BlueskyPostTests
             "Azure", "Identity", "WebAPI", "MSAL", "ManagedIdentity", "Entra"
         };
         
-        postBuilder.Append(" " + new Link(shortenedUrl, shortenedUrl));
+        postBuilder.Append(new Link(shortenedUrl, shortenedUrl));
+        
         // Get the OpenGraph info to embed
         var embeddedExternalRecord = await _blueskyManager.GetEmbeddedExternalRecord(url);
         if (embeddedExternalRecord != null)
@@ -92,7 +114,48 @@ public class BlueskyPostTests
         
         foreach (var hashtag in hashTags)
         {
-            postBuilder.Append(" " + new HashTag(hashtag));
+            postBuilder.Append(" ");
+            postBuilder.Append(new HashTag(hashtag));
+        }
+            
+        // Act
+        var response = await _blueskyManager.Post(postBuilder);
+        
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotNull(response.Cid);
+        
+        // Clean up
+        await _blueskyManager.DeletePost(response.StrongReference);
+    }
+    
+    [Fact]
+    public async Task SendBlueskyPostWithLinksAndLimitedHashTagsAndEmbedded_Success()
+    {
+        // Arrange
+        const string message = "ICYMI: (07/12/2024): \"My JetBrains Rider Plugins — July 2024 Edition\" RPs and feedback are always appreciated! ";
+        var postBuilder = new PostBuilder(message);
+        const string url = "https://www.josephguadagno.net/2024/07/12/my-jetbrains-rider-plugins-july-2024-edition";
+        const string shortenedUrl = "https://jjg.me/3LmnPiF";
+        var hashTags = new List<string>
+        {
+            "JetBrains", "Plugin", "presenting", "Rider"
+        };
+
+        postBuilder.Append(" ");
+        postBuilder.Append(new Link(shortenedUrl, shortenedUrl));
+        
+        // Get the OpenGraph info to embed
+        var embeddedExternalRecord = await _blueskyManager.GetEmbeddedExternalRecord(url);
+        if (embeddedExternalRecord != null)
+        {
+            postBuilder.EmbedRecord(embeddedExternalRecord);
+        }
+        
+        foreach (var hashtag in hashTags)
+        {
+            postBuilder.Append(" ");
+            postBuilder.Append(new HashTag(hashtag));
         }
             
         // Act
