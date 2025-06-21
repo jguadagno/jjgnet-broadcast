841e139e4bd0750626eb57087be2dcea8f76fce9
Joseph Guadagno <jguadagno@hotmail.com>
2025-06-20 18:04:50 -0700

    Removed the attempt to renew Client ID/Secrets

--- src/JosephGuadagno.Broadcasting.Managers.MicrosoftGraph/Interfaces/IMicrosoftGraphManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Graph.Models;
-
-namespace JosephGuadagno.Broadcasting.Managers.MicrosoftGraph.Interfaces;
-
-public interface IMicrosoftGraphManager
-{
-    public Task<List<User>> GetUsers(Models.ClientSecretCredential credentials);
-}
